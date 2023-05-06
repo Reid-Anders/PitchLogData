e3ee4a276250231eacccbb59be6919663d7ae5dc
mondayscool <76455195+mondayscool@users.noreply.github.com>
2023-05-06 08:15:37 -0700

    added grades db set

--- PitchLogContext.cs
+++ PitchLogContext.cs
@@ -25,6 +25,8 @@ namespace PitchLogData
 
         public DbSet<User> Users { get; set; }
 
+        public DbSet<Grade> Grades { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PitchLogDB")
