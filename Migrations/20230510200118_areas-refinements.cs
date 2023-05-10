c34fe83e3b16ab7e63ad70b1d58d07bb3f72bbf8
mondayscool <76455195+mondayscool@users.noreply.github.com>
2023-05-10 13:02:22 -0700

    add migration for refinining area entity

---
+++ Migrations/20230510200118_areas-refinements.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PitchLogData.Migrations
+{
+    public partial class areasrefinements : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NearestTown",
+                table: "Areas",
+                newName: "Region");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Areas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Municipality",
+                table: "Areas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Areas");
+
+            migrationBuilder.DropColumn(
+                name: "Municipality",
+                table: "Areas");
+
+            migrationBuilder.RenameColumn(
+                name: "Region",
+                table: "Areas",
+                newName: "NearestTown");
+        }
+    }
+}
